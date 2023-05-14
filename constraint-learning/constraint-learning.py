aa7a66bcad1aad1c319cd8ebfdefcb5d0af22726
tekeuange23 <tekeuangeleonel@gmail.com>
2023-05-14 23:55:04 +0100

    fixure: fix PCs plot

---
+++ constraint-learning/constraint-learning.py
@@ -0,0 +1,9 @@
+import warnings
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
+
+
+pd.set_option('display.max_columns', None)
+warnings.filterwarnings('ignore')
