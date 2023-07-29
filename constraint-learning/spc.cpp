9db919c197a8540fea4372c105292fd61e90bbd3
tekeuange23 <tekeuangeleonel@gmail.com>
2023-07-29 15:43:33 +0100

    add: creating CNF 2D vector from .cnf file parsing

--- constraint-learning/spc.cpp
+++ constraint-learning/spc.cpp
@@ -1,10 +1,100 @@
 #include <cryptominisat5/cryptominisat.h>
 #include <assert.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string.h>
 #include <vector>
-using std::vector;
+// using std::vector;
 using namespace CMSat;
+using namespace std;
 
-int main()
+vector<int> parse_cast_and_convert_to_vect(string str)
+{
+  // Create a vector to store the integers
+  vector<int> vect;
+
+  // Iterate over the string and parse each integer
+  for (int i = 0; i < str.length(); i++)
+  {
+    // Check if the current character is a digit
+    if (isdigit(str[i]) || str[i] == '-')
+    // if (isdigit(str[i]))
+    {
+      // Create a string to store the current integer
+      string num = "";
+
+      // Iterate over the next few characters and append them to the string
+      for (int j = i; j < str.length() && (isdigit(str[j]) || str[j] == '-'); j++)
+      {
+        num += str[j];
+        i = j;
+      }
+
+      // Convert the string to an integer and store it in the vector
+      // int value = stoi(num);
+      stringstream ss;
+      int value = 0;
+      ss << num;
+      ss >> value;
+      vect.push_back(value);
+    }
+  }
+
+  // Print the vector
+  // for (int i = 0; i < vect.size(); i++)
+  // {
+  //   cout << vect[i] << " ";
+  // }
+
+  return vect;
+}
+
+vector<vector<int>> readfile(string filename)
+{
+  // Create a 2D vector of shape (line, columns) = (20, 2318)
+  vector<vector<int>> data(0, vector<int>(2318));
+
+  // Open the file
+  fstream file(filename);
+
+  // Iterate from the 2nd line of the file to the end
+  if (file)
+  {
+    string line;
+    int i = 0;
+    getline(file, line);
+
+    // 2nd line of the file
+    while (getline(file, line))
+    {
+      // if (i > 0)
+      // {
+      //   break;
+      // }
+
+      // Store the line as a row of the 2D vector
+      data.push_back(parse_cast_and_convert_to_vect(line));
+
+      i++;
+    }
+  }
+  else
+  {
+    cout << "ERROR." << endl;
+  }
+
+  // Close the file
+  file.close();
+
+  cout << data.size() << endl;
+  for (int i = 0; i < data.size(); i++)
+    cout << data[i].size() << " | ";
+
+  return data;
+}
+
+int sat()
 {
   SATSolver solver;
   vector<Lit> clause;
@@ -66,6 +156,16 @@ int main()
   solver.add_clause(clause);
   ret = solver.solve();
   assert(ret == l_False);
+
+  return 0;
+}
+
+int main()
+{
+  // parse_cast_and_convert_to_vect("-1 2 3 4 5 6 7 -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -18 -19 -20 -21 -22 -23 -24 -25 -26 -27");
+
+  string filename = "/home/ange/WORKSPACE/AI/ml-in-action/constraint-learning/data/nsl-kdd.cnf";
+  readfile(filename);
 
   return 0;
 }
