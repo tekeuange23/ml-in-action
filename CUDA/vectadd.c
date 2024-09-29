8138afb46e108945f38db3b6a62e381f847ee7e8
tekeuange23 <tekeuangeleonel@gmail.com>
2024-09-29 21:17:13 +0100

    Host computes vector C = A+B with size=1000000000

--- CUDA/vectadd.c
+++ CUDA/vectadd.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+// #define N 1000000000
 #define N 10
 
-// Compute vector sum C = A+B
-void vecAdd(float *C, float *A, float *B, int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        C[i] = A[i] + B[i];
-}
 
 // Initialization
 float* vectInit(float value, size_t size){
@@ -31,15 +25,21 @@ float* vectInit(float value, size_t size){
     return vect;
 }
 
+// Compute vector sum C = A+B
+void vecAdd(float *C, float *A, float *B, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        C[i] = A[i] + B[i];
+}
+
 // Print the array elements
-void vectPrint(float *vect)
+void vectPrint(float *vect, size_t size)
 {
     int i;
     printf("-->: ");
-    for (i = 0; i < 10; i++)
-    {
+    for (i = 0; i < size; i++)
         printf("%f ", vect[i]);
-    }
     printf("\n");
 }
 
@@ -47,23 +47,26 @@ int main()
 {
 
     // Init
-    float *A, *B, *C;
-    A = vectInit(1.0, N);
-    B = vectInit(0.1, N);
-    C = vectInit(0.0, N);
+    float *h_A, *h_B, *h_C;
+    h_A = vectInit(1.0, N);
+    h_B = vectInit(0.1, N);
+    h_C = vectInit(0.0, N);
 
-    // Vect ddition
-    vecAdd(C, A, B, N);
+    /** *************************** */
+    /**         Vect Addition       */
+    /** *************************** */
+    vecAdd(h_C, h_A, h_B, N);    // */
+    /** *************************** */
 
     // Show
-    vectPrint(A);
-    vectPrint(B);
-    vectPrint(C);
+    vectPrint(h_A, N);
+    vectPrint(h_B, N);
+    vectPrint(h_C, N);
 
     // Free memory
-    free(A);
-    free(B);
-    free(C);
+    free(h_A);
+    free(h_B);
+    free(h_C);
 
     return 0;
 }
