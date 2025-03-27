743451de9bcd37e7039549d0927b1184bdd5567b
tekeuange23 <tekeuangeleonel@gmail.com>
2025-03-27 23:03:52 +0100

    Enhance Perceptron implementation with decision region plotting and MSE calculation; update training iterations and add data loading functionality

--- Torch/ANN/perceptron.py
+++ Torch/ANN/perceptron.py
@@ -1,21 +1,23 @@
 import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 class Perceptron:
     """Perceptron classifier.
-    
+
     Parameters
     ------------
-    
     eta : float
         Learning rate (between 0.0 and 1.0)
     n_iter : int
         Passes over the training dataset.
     random_state : int
         Random number generator seed for random weight initialization.
-    
+
     Attributes
     -----------
-    
     w_ : 1d-array
         Weights after fitting.
     b_ : Scalar
@@ -23,11 +25,14 @@ class Perceptron:
     errors_ : list
         Number of misclassifications (updates) in each epoch.
     """
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    def __init__(self, eta=0.01, n_iter=2000000000, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
-        
+        self.w_ = None
+        self.b_ = None
+        self.errors_ = None
+
     def fit(self, X, y):
         """Fit training data.
         
@@ -43,18 +48,20 @@ class Perceptron:
         self : object
         """
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01,
-        size=X.shape[1])
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
         self.b_ = np.float_(0.)
         self.errors_ = []
         for _ in range(self.n_iter):
-            errors = 0
+            mse = 0
             for xi, target in zip(X, y):
+                # MSE
+                n = X.shape[0]
+                delta = 2/n * self.eta * (target - self.predict(xi))
                 update = self.eta * (target - self.predict(xi))
-                self.w_ += update * xi
-                self.b_ += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+                self.w_ += delta * xi
+                self.b_ += delta
+                mse += int(delta != 0.0)
+            self.errors_.append(mse)
         return self
             
             
@@ -65,7 +72,68 @@ class Perceptron:
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, 0)
-    
 
-if __name__ == 'main':
-    print('hi')
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+    np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+        y=X[y == cl, 1],
+        alpha=0.8,
+        c=colors[idx],
+        marker=markers[idx],
+        label=f'Class {cl}',
+        edgecolor='black')
+
+if __name__ == '__main__':
+    # loading the data
+    df = pd.read_csv('datasets/iris.data.csv', header = None, encoding = 'utf-8')
+    print(df.iloc[:50].tail())
+    print(df.iloc[50:100].head())
+
+    # select setosa and versicolor
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', 0, 1)
+    
+    # extract sepal length and petal length
+    # X = df.iloc[0:100, [0, 1, 2, 3]].values
+    X = df.iloc[0:100, [0, 2]].values
+    
+    # plot data
+    plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'Setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 's', label = 'Versicolor')
+    plt.xlabel('Sepal length [cm]')
+    plt.ylabel('Petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    # training
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X, y)
+    plt.plot(range(1, len(ppn.errors_) + 1),
+    ppn.errors_, marker = 'o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of updates')
+    plt.show()
+    
+    # A plot of the perceptron’s decision regions
+    plot_decision_regions(X, y, classifier=ppn)
+    plt.xlabel('Sepal length [cm]')
+    plt.ylabel('Petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
