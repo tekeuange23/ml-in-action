c662b636a43b8326a4f3f516c101fe8a8449acf4
tekeuange23 <tekeuangeleonel@gmail.com>
2025-08-25 12:59:26 +0100

    start cuda essential: indexing grid,block,wraps,threads

---
+++ CUDA/essential/01.indexing.cu
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+__global__ void whoami(void) {
+  int block_id =
+    blockIdx.x +    // apartment number on this floor (points across)
+    blockIdx.y * gridDim.x +    // floor number in this building (rows high)
+    blockIdx.z * gridDim.x * gridDim.y;   // building number in this city (panes deep)
+
+  int block_offset =
+    block_id * // times our apartment number
+    blockDim.x * blockDim.y * blockDim.z; // total threads per block (people per apartment)
+
+  int thread_offset =
+    threadIdx.x +
+    threadIdx.y * blockDim.x +
+    threadIdx.z * blockDim.x * blockDim.y;
+
+  int id = block_offset + thread_offset; // global person id in the entire apartment complex
+
+  printf("%04d | Block(%d %d %d) = %3d | Thread(%d %d %d) = %3d\n",
+    id,
+    blockIdx.x, blockIdx.y, blockIdx.z, block_id,
+    threadIdx.x, threadIdx.y, threadIdx.z, thread_offset);
+  // printf("blockIdx.x: %d, blockIdx.y: %d, blockIdx.z: %d, threadIdx.x: %d, threadIdx.y: %d, threadIdx.z: %d\n", blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z);
+}
+
+
+int main(int argc, char** argv) {
+  const int b_x = 2, b_y = 3, b_z = 4;
+  const int t_x = 4, t_y = 4, t_z = 4; // the max warp size is 32, so 
+  // we will get 2 warp of 32 threads per block
+
+  int blocks_per_grid = b_x * b_y * b_z;
+  int threads_per_block = t_x * t_y * t_z;
+
+  printf("%d blocks/grid\n", blocks_per_grid);
+  printf("%d threads/block\n", threads_per_block);
+  printf("%d warps/block\n", threads_per_block / 32);
+  printf("%d total threads\n", blocks_per_grid * threads_per_block);
+
+  dim3 blocksPerGrid(b_x, b_y, b_z); // 3d cube of shape 2*3*4 = 24
+  dim3 threadsPerBlock(t_x, t_y, t_z); // 3d cube of shape 4*4*4 = 64
+
+  whoami<<<blocksPerGrid, threadsPerBlock >>> ();
+  cudaDeviceSynchronize();
+}
